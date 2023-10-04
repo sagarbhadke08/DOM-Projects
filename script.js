43292b6aa6ba3eb65511d4312e2e7afd94b9f3ed
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-10-04 22:38:11 +0530

    Updated

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const buttons =  document.querySelectorAll('.button')
-console.log(buttons)
+// console.log(buttons)
+const body = document.querySelector("body")
+console.log(body);
+
+buttons.forEach(function(button){
+    // console.log(button)
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+    })
+})
